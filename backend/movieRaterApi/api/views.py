553333fefc222d80dcad1dd7f6d0dc392555612b
Denis John <denis.john.ndarake@gmail.com>
2019-11-14 13:00:39 +0100

    Created new project with api app and also created serializers with movie and rating viewset

---
+++ backend/movieRaterApi/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from .serializers import MovieSerializer, RatingSerializer
+from .models import Movie, Rating
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
