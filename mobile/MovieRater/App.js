0c146303e25394689f5e70c725d6c0501d29ad43
Denis John <denis.john.ndarake@gmail.com>
2019-12-24 11:01:35 +0100

    Completed movie list and edit

--- mobile/MovieRater/App.js
+++ mobile/MovieRater/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-import { View } from "react-native";
 import MovieList from "./components/List";
+import Detail from "./components/Detail";
 
-export default function App() {
-	return <MovieList />;
-}
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Edit from "./components/Edit";
+
+const AppNavigator = createStackNavigator({
+	MovieList: { screen: MovieList },
+	Detail: { screen: Detail },
+	Edit: { screen: Edit },
+});
+
+const App = createAppContainer(AppNavigator);
+
+export default App;
+
+// export default function App() {
+// 	return <MovieList />;
+// }
--- mobile/MovieRater/components/List.js
+++ mobile/MovieRater/components/List.js
@@ -1,13 +1,56 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	FlatList,
+	Image,
+	TouchableOpacity,
+} from "react-native";
 
-export default function MovieList() {
+const MovieList = props => {
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		fetch("http://192.168.88.14:8000/api/movies/", {
+			method: "GET",
+			headers: {
+				Authorization: `Token 8323e066366f6ec79bb0555dd6cc49172b12d600`,
+			},
+		})
+			.then(res => res.json())
+			.then(jsonRes => setMovies(jsonRes))
+			.catch(e => console.log(e));
+	}, []);
+
+	const movieClicked = movie => {
+		props.navigation.navigate("Detail", { movie: movie, title: movie.title });
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>This will display my list</Text>
+		<View>
+			<Image
+				source={require("../assets/MR_logo.png")}
+				style={{ width: "100%", height: 135, paddingTop: 20 }}
+				resizeMode='cover'
+			/>
+
+			<FlatList
+				data={movies}
+				renderItem={({ item }) => (
+					<TouchableOpacity onPress={() => movieClicked(item)}>
+						<View style={styles.item}>
+							<Text style={styles.itemText}>{item.title}</Text>
+						</View>
+					</TouchableOpacity>
+				)}
+				keyExtractor={(item, index) => index.toString()}
+			/>
 		</View>
 	);
-}
+};
+
+export default MovieList;
 
 const styles = StyleSheet.create({
 	container: {
@@ -15,5 +58,15 @@ const styles = StyleSheet.create({
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
+	},
+	item: {
+		flex: 1,
+		padding: 10,
+		height: 50,
+		backgroundColor: "#282C35",
+	},
+	itemText: {
+		color: "#fff",
+		fontSize: 25,
 	},
 });
